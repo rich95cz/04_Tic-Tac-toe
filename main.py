0c06430609a088e43439299f1c7c31e7fc25aaf5
rich95cz <richard.papez.ml@gmail.com>
2025-05-15 01:48:11 +0200

    Přidán popis funkcí, drobné úpravy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-projekt_1.py: první projekt do Engeto Online Python Akademie
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
 
 author: Richard Papež
 email: richard.papez.ml@gmail.com¨
@@ -10,25 +10,13 @@ from copy import deepcopy
 from colorama import init, Fore, Style
 from time import sleep
 
-# Proměnné
-stav_hry_novy = {"na tahu": "o",
-            "pole x": list(),
-            "pole o": list(),
-            "kolo": 1, 
-            "pole 1": " ",
-            "pole 2": " ",
-            "pole 3": " ",
-            "pole 4": " ",
-            "pole 5": " ",
-            "pole 6": " ",
-            "pole 7": " ",
-            "pole 8": " ",
-            "pole 9": " ",
-            "dohrano": "ne"
-            }
-
 # Funkce
 def zobraz_uvod():
+    """
+    Tiskne úvod zkopírovaný ze zadání projektu.
+
+    Bez vstupu či výstupu.
+    """
     uvod_text = """Welcome to Tic Tac Toe
 ============================================
 GAME RULES:
@@ -46,6 +34,17 @@ Let's start the game
     print(uvod_text)
 
 def ziskej_volbu(stav):
+    """
+    Ptá se hráče, na jaké pole chce umístit hrací kámen,
+    kontroluje vstup a přepisuje stav hry:
+        - připisuje položený kámen hráči
+        - obsazuje pole
+        - přičítá kolo
+
+    :param stav: slovník obsahující aktuální stav hry
+    :type stav: dict
+    Bez návratové funkce.
+    """
     oddelovac = "=" * 44
     pripustne_volby = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     print(oddelovac)
@@ -56,17 +55,29 @@ def ziskej_volbu(stav):
     stav[f"pole {stav['na tahu']}"].append(zadane_policko)
     stav[f"pole {zadane_policko}"] = stav["na tahu"]
     stav["kolo"] += 1
-    return stav
 
 def zmen_hrace(stav):
+    """
+    Mění aktuálně táhnoucího hráče. Pokud už někdo vyhrál, funkce rovnou skončí.
+
+    :param stav: slovník obsahující aktuální stav hry
+    :type stav: dict
+    Bez návratové hodnoty.
+    """
     if stav["dohrano"] == "ano":
-        return stav
+        return
     if stav["na tahu"] == "o":
         stav["na tahu"] = "x"
     else: stav["na tahu"] = "o"
-    return stav
 
 def vykresli_herni_plochu(stav):
+    """
+    Vykresluje aktuální hrací plochu.
+
+    :param stav: slovník obsahující aktuální stav hry
+    :type stav: dict
+    Bez návratové funkce.
+    """
     oddelovac = "=" * 44
     if stav["kolo"] != 1:
         print(oddelovac)
@@ -81,6 +92,13 @@ def vykresli_herni_plochu(stav):
     )
 
 def vyhodnot_vyhru(stav):
+    """
+    Ověřuje, zda již někdo vyhrál, a přepisuje stav hry, pokud ano.
+
+    :param stav: slovník obsahující aktuální stav hry
+    :type stav: dict
+    Bez návratové funkce.
+    """
     overit = stav['na tahu']
 
     vyhra_1 = (stav["pole 1"] == overit, stav["pole 2"] == overit, stav["pole 3"] == overit)
@@ -100,7 +118,15 @@ def vyhodnot_vyhru(stav):
         print(oddelovac + Fore.GREEN + f"\nCongratulations, the player {stav['na tahu']} WON!\n" + Style.RESET_ALL + oddelovac)
         stav["dohrano"] = "ano"
 
-def dej_novou_hru(stav):
+def dej_novou_hru(stav, stav_novy):
+    """
+    Ptá se hráče, jestli chce hrát další partii. Pokud ano, vynuluje stav hry. Pokud ne, ukončí aplikaci.
+
+    :param stav: slovník obsahující aktuální stav hry
+    :type stav: dict
+    :return: slovník obsahující nový stav hry
+    :rtype: dict
+    """
     oddelovac = "=" * 44
     pripustne_volby = ["y", "n"]
     if stav["dohrano"] == "ne":
@@ -112,28 +138,52 @@ def dej_novou_hru(stav):
     if nova_hra == "y":
         print(Fore.BLUE + "\nLet's play a new one !\n" + Style.RESET_ALL)
         sleep(2)
-        return stav_hry_novy
+        return stav_novy
     else:
         print("\nSee you next time...")
         sleep(2)
         exit()
 
 # Hlavní funkce
-def main(stav: dict = stav_hry_novy):
+def main():
+    """
+    Hlavní funkce. Definuje výchozí stav hry, inicializuje coloramu, obsahuje hlavní herní smyčku.
+    Je tu víceméně jen proto, aby skript neobsahoval globální proměnné.
+
+    Bez vstupu či výstupu.
+    """
+    stav_hry_novy = {"na tahu": "o",
+            "pole x": list(),
+            "pole o": list(),
+            "kolo": 1, 
+            "pole 1": " ",
+            "pole 2": " ",
+            "pole 3": " ",
+            "pole 4": " ",
+            "pole 5": " ",
+            "pole 6": " ",
+            "pole 7": " ",
+            "pole 8": " ",
+            "pole 9": " ",
+            "dohrano": "ne"
+            }
+    
     init()
-    stav_hry = deepcopy(stav)
+
+    stav_hry = deepcopy(stav_hry_novy)
+
     while True:
         if stav_hry["kolo"] == 1:
             zobraz_uvod()
             vykresli_herni_plochu(stav_hry)
-        stav_hry = ziskej_volbu(stav_hry)
+        ziskej_volbu(stav_hry)
         vykresli_herni_plochu(stav_hry)
         vyhodnot_vyhru(stav_hry)
-        stav_hry = zmen_hrace(stav_hry)
+        zmen_hrace(stav_hry)
         if stav_hry["dohrano"] == "ano":
-            stav_hry = deepcopy(dej_novou_hru(stav_hry))
+            stav_hry = deepcopy(dej_novou_hru(stav_hry, stav_hry_novy))
         elif stav_hry["kolo"] > 9:
-            stav_hry = deepcopy(dej_novou_hru(stav_hry))
+            stav_hry = deepcopy(dej_novou_hru(stav_hry, stav_hry_novy))
 
 # Aplikace
 main()
